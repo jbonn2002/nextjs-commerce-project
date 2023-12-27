4a0d642aa83b1e47b6d8a77f3f01949e24f545e7
Jordy <jbonn2002@gmail.com>
2023-12-26 19:27:57 -0800

    changed edit item quantity plus and minus signs color and hover

--- components/cart/edit-item-quantity-button.tsx
+++ components/cart/edit-item-quantity-button.tsx
@@ -20,7 +20,7 @@ function SubmitButton({ type }: { type: 'plus' | 'minus' }) {
       aria-label={type === 'plus' ? 'Increase item quantity' : 'Reduce item quantity'}
       aria-disabled={pending}
       className={clsx(
-        'ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80',
+        'ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-70',
         {
           'cursor-not-allowed': pending,
           'ml-auto': type === 'minus'
@@ -28,11 +28,11 @@ function SubmitButton({ type }: { type: 'plus' | 'minus' }) {
       )}
     >
       {pending ? (
-        <LoadingDots className="bg-black dark:bg-white" />
+        <LoadingDots className="bg-white" />
       ) : type === 'plus' ? (
-        <PlusIcon className="h-4 w-4 dark:text-neutral-500" />
+        <PlusIcon className="h-4 w-4" />
       ) : (
-        <MinusIcon className="h-4 w-4 dark:text-neutral-500" />
+        <MinusIcon className="h-4 w-4" />
       )}
     </button>
   );
